024302f5c0538f8936abbf41b30c2ad2c66c73e8
Alex Spitzer <aes368@cornell.edu>
2014-02-15 02:51:25 -0500

    Working chebSine
    Slower than machine sine :(
     - 4.3 s vs 3 s
    
    Also error near 2*PI is very big (max error is 10)
     - performs much better in the rest of the range
    
    Added test.cpp file
     - compile with g++ test.cpp
     - run with ./a.out

--- chebSine.h
+++ chebSine.h
@@ -4,6 +4,7 @@
 #include <cmath>
 #include <iostream>
 #include <fstream>
+#include <sstream>
 #include <unistd.h>
 
 using namespace std;
@@ -12,64 +13,73 @@ class ChebSine : public SinePrototype
 {
     public:
         int **MAT;
-        int *C;
+        double *C;
         int N;
+        double *powerX;
 
         // Overload the constructor
         ChebSine(const int n)
         {
-            system("python chebygen 10");
-            sleep(1);
-            parseFile("matrix.txt");
             N = n;
-            // When you overload this constructor, use it to initialize constants
-            // and array storage 
+            powerX = new double[N];
+            stringstream ss;
+            ss << "python cheby_gen.py ";
+            ss << n;
+            string s = ss.str();
+
+            system(s.c_str());
+            sleep(0.5);
+            parseFile("coeffs.dat");
         }
 
         ~ChebSine() {
             delete []MAT;
+            delete C;
+            delete powerX;
         }
 
         void parseFile(string filename) {
             ifstream file(filename.c_str());
+
             MAT = new int*[N];
             for (int i = 0; i < N; i++) {
-                MAT[i] = new int[i];
+                MAT[i] = new int[N];
             }
-            int t;
-            for (int i = 0; i < N; i++) {
-                for (int j = 0; j < N; j++) {
-                    file >> t;
-                    MAT[i][j] = t;
-                    cout << MAT[i][j] << " " ;
-                }
-                cout << endl;
-            }
+
+            for (int i = 0; i < N; i++)
+                for (int j = 0; j < N; j++)
+                    file >> MAT[i][j];
+
+            C = new double[N];
+            for (int i = 0; i < N; i++)
+                file >> C[i];
         }
 
 
         // Overload the function with your function
         virtual double sine(double x) {
-            double powerX[N];
-            powerX[0] = 1;
-            for(int  i = 1; i < N ; i++ ){
-                powerX[i] = powerX[i-1]*x; 
+            double curr = 1.0;
+            powerX[0] = curr;
+            for (int i = 1; i < N ; i++) {
+                curr *= x;
+                powerX[i] = curr;
             }
 
             double sineX = 0.0;
             double sum = 0.0;
-            int ref = 0;
+            int start = 0;
 
-            for(int j = 0; j < N ; j++){
-                for(int k=0; k < (j/2 + 1); k++){
-                    ref = k*2 + j%2; //This takes care of the odd-even alternative multiplication
-                    sum += MAT[j][ref]*powerX[ref];
+            for (int j = 0; j < N ; j++) {
+                int *row = MAT[j];
+                for (int k = start; k <= j; k += 2) {
+                    sum += row[k] * powerX[k];
                 }
                 sineX += C[j]*sum;
                 sum = 0;
+                start = 1 - start;
             }
 
-            return sineX;
+            return sineX - C[0] / 2.0;
         }
 };
 #endif
